cf2ebc04705421b8a9a9050f8d88bdb69a54e48b
ww <307763103@qq.com>
2017-12-23 19:26:22 +0800

    第一次项目提交

---
+++ test/src/main/java/com/ww/test/Test.java
@@ -0,0 +1,33 @@
+/**   
+ * @Title: Test.java 
+ * @Package com.ww.test 
+ * @Description: TODO(用一句话描述该文件做什么) 
+ * @author 王伟 
+ * @date 2017年12月23日 下午7:24:18 
+ * @version V1.0   
+ */
+
+package com.ww.test;
+
+/** 
+ * @ClassName: Test 
+ * @Description: TODO(这里用一句话描述这个类的作用) 
+ * @author 王伟 
+ * @date 2017年12月23日 下午7:24:18 
+ *  
+ */
+public class Test {
+
+	/** 
+	 * @Title: main 
+	 * @Description: TODO(这里用一句话描述这个方法的作用) 
+	 * @param args    设定文件 
+	 * @return void    返回类型 
+	 * @throws 
+	 */
+	public static void main(String[] args) {
+		System.out.println("你好");
+
+	}
+
+}
